0eb78887c3c2d0dd93241c8a72dace8a649c862c
Ben <bxc3201@rit.edu>
2017-04-01 21:52:27 -0400

    initial

---
+++ client/update.js
@@ -0,0 +1,122 @@
+//when we receive a character update
+const update = (data) => {
+  //if we do not have that character (based on their id)
+  //then add them
+  if(!squares[data.hash]) {
+    squares[data.hash] = data;
+    return;
+  }
+
+  //if the update is for our own character (we dont need it)
+  //Although, it could be used for player validation
+  if(data.hash === hash) {
+    //return; now we are listening for our own update
+  }
+
+  //if we received an old message, just drop it
+  if(squares[data.hash].lastUpdate >= data.lastUpdate) {
+    return;
+  }
+
+  //grab the character based on the character id we received
+  const square = squares[data.hash];
+  //update their direction and movement information
+  //but NOT their x/y since we are animating those
+  square.prevX = data.prevX;
+  square.prevY = data.prevY;
+  square.x = data.destX;
+  square.y = data.destY;
+  square.vx = data.vx;
+  square.vy = data.vy;
+  square.direction = data.direction;
+  square.moveLeft = data.moveLeft;
+  square.moveRight = data.moveRight;
+  square.moveDown = data.moveDown;
+  square.moveUp = data.moveUp;
+  square.alpha = 0.05;
+};
+
+//function to remove a character from our character list
+const removeUser = (data) => {
+  //if we have that character, remove them
+  if(squares[data.hash]) {
+    delete squares[data.hash];
+  }
+};
+
+//function to set this user's character
+const setUser = (data) => {
+  hash = data.hash; //set this user's hash to the unique one they received
+  squares[hash] = data; //set the character by their hash
+  requestAnimationFrame(redraw); //start animating
+};
+
+//when receiving an attack (cosmetic, not collision event)
+//add it to our attacks to draw
+const receiveAttack = (data) => {
+  attacks.push(data);
+};
+
+//function to send an attack request to the server
+const sendAttack = () => {
+  const square = squares[hash];
+  
+  //create a new attack in a certain direction for this user
+  const attack = {
+    hash: hash,
+    x: square.x,
+    y: square.y,
+    direction: square.direction,
+    frames: 0,
+  }
+  
+  //send request to server
+  socket.emit('attack', attack);
+};
+
+//when a character is killed
+const playerDeath = (data) => {
+  //remove the character
+  delete squares[data];
+  
+  //if the character killed is our character
+  //then disconnect and draw a game over screen
+  if(data === hash) {
+    socket.disconnect();
+    cancelAnimationFrame(animationFrame);
+    ctx.fillRect(0, 0, 500, 500);
+    ctx.fillStyle = 'white';
+    ctx.font = '48px serif';
+    ctx.fillText('You died', 50, 100);
+  }
+};
+
+//update this user's positions based on keyboard input
+const updatePosition = () => {
+  const square = squares[hash];
+  
+  // handle position change on server
+
+  //determine direction based on the inputs of direction keys
+  if(square.moveUp && square.moveLeft) square.direction = directions.UPLEFT;
+
+  if(square.moveUp && square.moveRight) square.direction = directions.UPRIGHT;
+
+  if(square.moveDown && square.moveLeft) square.direction = directions.DOWNLEFT;
+
+  if(square.moveDown && square.moveRight) square.direction = directions.DOWNRIGHT;
+
+  if(square.moveLeft && !(square.moveUp || square.moveDown || square.moveRight)) square.direction = directions.LEFT;
+
+  if(square.moveRight && !(square.moveUp || square.moveDown || square.moveLeft)) square.direction = directions.RIGHT;
+  
+  if(square.moveDown && !(square.moveRight || square.moveLeft)) square.direction = directions.DOWN;
+
+  if(square.moveUp && !(square.moveRight || square.moveLeft)) square.direction = directions.UP;
+
+  //reset this character's alpha so they are always smoothly animating
+  square.alpha = 0.05;
+
+  //send the updated movement request to the server to validate the movement.
+  socket.emit('movementUpdate', square);
+};
---
+++ server/physics.js
@@ -0,0 +1,161 @@
+// our socket code for physics to send updates back
+const sockets = require('./sockets.js');
+
+let charList = {}; // list of characters
+const attacks = []; // array of attack to handle
+
+// box collision check between two rectangles
+// of a set width/height
+const checkCollisions = (rect1, rect2, width, height) => {
+  if (rect1.x < rect2.x + width &&
+     rect1.x + width > rect2.x &&
+     rect1.y < rect2.y + height &&
+     height + rect1.y > rect2.y) {
+    return true; // is colliding
+  }
+  return false; // is not colliding
+};
+
+// check attack collisions to see if colliding with the
+// user themselves and return false so users cannot damage
+// themselves
+const checkAttackCollision = (character, attackObj) => {
+  const attack = attackObj;
+
+  // if attacking themselves, we won't check collision
+  if (character.hash === attack.hash) {
+    return false;
+  }
+
+  // otherwise check collision of user rect and attack rect
+  return checkCollisions(character, attack, attack.width, attack.height);
+};
+
+const movePlayer = (hash) => {
+  let square = charList[hash];
+  
+  if (!square) return;
+
+  //move the last x/y to our previous x/y variables
+  square.prevX = square.x;
+  square.prevY = square.y;
+
+  //if user is jumping, add jump force
+  if(square.moveUp && square.vy >= 0) {
+    square.vy = -10;
+  }
+  //if user is moving down, ignore
+  if(square.moveDown) {
+    //square.vy = 2; let gravity handle it
+  }
+  //if user is moving left, decrease x velocity
+  if(square.moveLeft) {
+    square.vx = -5;
+  }
+  //if user is moving right, increase x velocity
+  if(square.moveRight) {
+    square.vx = 5;
+  }
+  
+  if (!square.moveRight && !square.moveLeft) {
+    square.vx = 0;
+  }
+  
+  addGravity(square);
+  
+  // add velocity with dt to get desired position
+  updatePosition(square)
+  
+  // keep the character in bounds
+  clampPosToBounds(square);
+}
+
+const addGravity = (character) => {
+  character.vy += 1;
+}
+
+const updatePosition = (character) => {
+  character.destY = character.prevY + character.vy;
+  character.destX = character.prevX + character.vx;
+}
+
+const clampPosToBounds = (character) => {
+  //if user is moving up, decrease y
+  if(character.destY < 0) {
+    character.destY = 0;
+  }
+  //if user is moving down, increase y
+  if(character.destY > 400) {
+    character.destY = 400;
+  }
+  //if user is moving left, decrease x
+  if(character.destX < 0) {
+    character.destX = 0;
+  }
+  //if user is moving right, increase x
+  if(character.destX > 400) {
+    character.destX = 400;
+  }
+}
+
+// handle each attack and calculate collisions
+const checkAttacks = () => {
+  // if we have attack
+  if (attacks.length > 0) {
+    // get all characters
+    const keys = Object.keys(charList);
+    const characters = charList;
+
+    // for each attack
+    for (let i = 0; i < attacks.length; i++) {
+      // for each character
+      for (let k = 0; k < keys.length; k++) {
+        const char1 = characters[keys[k]];
+
+        // call to see if the attack and character hit
+        const hit = checkAttackCollision(char1, attacks[i]);
+
+        if (hit) { // if a hit
+          // ask sockets to notify users which character was hit
+          sockets.handleAttack(char1.hash);
+          // kill that character and remove from our user list
+          delete charList[char1.hash];
+        } else {
+          // if not a hit
+          console.log('miss');
+        }
+      }
+
+      // once the attack has been calculated again all users
+      // remove this attack and move onto the next one
+      attacks.splice(i);
+      // decrease i since our splice changes the array length
+      i--;
+    }
+  }
+};
+
+// update our entire character list
+const setCharacterList = (characterList) => {
+  charList = characterList;
+};
+
+// update an individual character
+const setCharacter = (character) => {
+  charList[character.hash] = character;
+};
+
+// add a new attack to calculate physics on
+const addAttack = (attack) => {
+  attacks.push(attack);
+};
+
+// check for collisions every 20ms
+setInterval(() => {
+  checkAttacks();
+}, 20);
+
+module.exports.setCharacterList = setCharacterList;
+module.exports.setCharacter = setCharacter;
+module.exports.addAttack = addAttack;
+module.exports.movePlayer = movePlayer;
